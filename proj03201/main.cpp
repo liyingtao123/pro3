7765701b0ca18e10ed582a14acb960e0a7a34f08
liyingtao123 <1320135362@qq.com>
2022-06-26 11:27:19 +0800

    commit fili to pro3

---
+++ proj03201/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include<vector>
+using namespace std;
+
+vector<int> test(vector<int> vec1)
+{
+    int n=vec1.size();
+    if(n>0)
+    {
+        vec1[0]=10;
+    }
+    for(auto val:vec1)
+    {
+        cout<<val<<" ";
+    }
+    cout<<endl;
+}
+
+int main()
+{
+    vector<int> vec2;
+    vec2.push_back(43);
+    vec2.push_back(11);
+    test(vec2);
+    for(auto val:vec2){
+        cout<<val<<" ";
+    }
+    return 0;
+}
